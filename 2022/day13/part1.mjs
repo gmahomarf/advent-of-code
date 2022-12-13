03946b0bc63749480c55bb717447daeb22418731
Gazy Mahomar <gmahomarf@gmail.com>
2022-12-13 11:32:15 +0100

    Added day13

---
+++ 2022/day13/part1.mjs
@@ -0,0 +1,60 @@
+import { createReadStream } from 'node:fs';
+import readline from 'node:readline';
+
+const input = readline.createInterface(createReadStream('input.txt', 'utf8'));
+// const input = readline.createInterface(createReadStream('input-ex.txt', 'utf8'));
+
+function compare(p1, p2) {
+    if (typeof p1 === 'number') {
+        if (typeof p2 === 'number') {
+            return p1 - p2;
+        }
+        return compare([p1], p2);
+    } else if (typeof p2 === 'number') {
+        return compare(p1, [p2]);
+    } else {
+        if (p2.length < p1.length) {
+            for (let i = 0; i < p2.length; i++) {
+                const r = compare(p1[i], p2[i]);
+                if (r) {
+                    return r;
+                }
+            }
+            return 0;
+        } else {
+            for (let i = 0; i < p1.length; i++) {
+                const r = compare(p1[i], p2[i]);
+                if (r) {
+                    return r;
+                }
+            }
+            return p1.length === p2.length ? 0 : -1;
+        }
+    }
+}
+
+async function run() {
+    const packetPairs = []
+    let c = 0;
+    for await (const line of input) {
+        if (line.trim().length) {
+            packetPairs[c] ||= [];
+            packetPairs[c].push(JSON.parse(line));
+        } else {
+            c++;
+        }
+    }
+
+    const orderedPairs = [];
+    for (const [i, [p1, p2]] of Object.entries(packetPairs)) {
+        if (compare(p1, p2) < 0) {
+            orderedPairs.push(+i + 1);
+        }
+    }
+
+    // console.log(packetPairs)
+    console.log(orderedPairs);
+    console.log(orderedPairs.reduce((a, b) => a + b));
+}
+
+run();
---
+++ 2022/day13/part2.mjs
@@ -0,0 +1,60 @@
+import { createReadStream } from 'node:fs';
+import readline from 'node:readline';
+
+const input = readline.createInterface(createReadStream('input.txt', 'utf8'));
+// const input = readline.createInterface(createReadStream('input-ex.txt', 'utf8'));
+
+function compare(p1, p2) {
+    if (typeof p1 === 'number') {
+        if (typeof p2 === 'number') {
+            return p1 - p2;
+        }
+        return compare([p1], p2);
+    } else if (typeof p2 === 'number') {
+        return compare(p1, [p2]);
+    } else {
+        if (p2.length < p1.length) {
+            for (let i = 0; i < p2.length; i++) {
+                const r = compare(p1[i], p2[i]);
+                if (r) {
+                    return r;
+                }
+            }
+            return 1;
+        } else {
+            for (let i = 0; i < p1.length; i++) {
+                const r = compare(p1[i], p2[i]);
+                if (r) {
+                    return r;
+                }
+            }
+            return p1.length === p2.length ? 0 : -1;
+        }
+    }
+}
+
+async function run() {
+    const packets = [[2], [6]]
+    const dividers = [];
+
+    for await (const line of input) {
+        if (line.trim().length) {
+            packets.push(JSON.parse(line));
+        }
+    }
+
+    packets.sort(compare);
+
+    for (const i in packets) {
+        if (compare([[2]], packets[i]) === 0 || compare([[6]], packets[i]) === 0) {
+            dividers.push(+i + 1);
+        }
+        if (dividers.length === 2)
+            break;
+    }
+
+    console.log(dividers);
+    console.log(dividers[0] * dividers[1]);
+}
+
+run();
