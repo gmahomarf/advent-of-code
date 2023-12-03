f2a9ee709b28934e28887e55d5fc676e0b38c517
Gazy Mahomar <gmahomarf@gmail.com>
2023-12-03 11:51:31 +0100

    2023 day 3

---
+++ 2023/day03/part1.mjs
@@ -0,0 +1,73 @@
+import { createReadStream } from "node:fs";
+import { createInterface } from 'node:readline'
+
+const input = createInterface(createReadStream('input.txt', 'utf8'));
+
+const numbers = [];
+const symbols = {};
+
+let height = 0;
+let width;
+const re = /(?<s>[^.\d])|(?<n>\d+)/g;
+
+for await (const line of input) {
+    let match;
+
+    while (match = re.exec(line)) {
+        if (match.groups.s) {
+            symbols[`${match.index}x${height}`] = match[0]
+        } else {
+            numbers.push({
+                y: height,
+                s: match.index,
+                e: match.index + match[0].length - 1,
+                n: +match[0]
+            })
+        }
+    }
+
+    height++;
+    width = line.length;
+}
+
+let sum = 0;
+main:
+for (const number of numbers) {
+    let y = number.y - 1;
+
+    if (y > 0) {
+        for (let x = number.s - 1; x <= number.e + 1; x++) {
+            if (x < 0 || x >= width) continue;
+            if (symbols[`${x}x${y}`]) {
+                sum += number.n;
+                continue main;
+            }
+        }
+    }
+
+    y++;
+    if (number.s > 0 && symbols[`${number.s - 1}x${y}`]) {
+        sum += number.n;
+        continue;
+    }
+
+    if (number.e < width - 1 && symbols[`${number.e + 1}x${y}`]) {
+        sum += number.n;
+        continue;
+    }
+
+    y++;
+    if (y >= height) {
+        continue;
+    }
+
+    for (let x = number.s - 1; x <= number.e + 1; x++) {
+        if (x < 0 || x >= width) continue;
+        if (symbols[`${x}x${y}`]) {
+            sum += number.n;
+            continue main;
+        }
+    }
+}
+
+console.log(sum)
---
+++ 2023/day03/part2.mjs
@@ -0,0 +1,60 @@
+import { createReadStream } from "node:fs";
+import { createInterface } from 'node:readline'
+
+const input = createInterface(createReadStream('input.txt', 'utf8'));
+
+const numbers = [];
+const gears = {};
+
+let height = 0;
+let width;
+const re = /(?<s>[^.\d])|(?<n>\d+)/g;
+
+for await (const line of input) {
+    let match;
+
+    while (match = re.exec(line)) {
+        if (match.groups.s === '*') {
+            gears[`${match.index}x${height}`] = []
+        } else {
+            numbers.push({
+                y: height,
+                s: match.index,
+                e: match.index + match[0].length - 1,
+                n: +match[0]
+            })
+        }
+    }
+
+    height++;
+    width = line.length;
+}
+
+for (const number of numbers) {
+    let y = number.y - 1;
+    if (y > 0) {
+        for (let x = number.s - 1; x <= number.e + 1; x++) {
+            if (x < 0 || x >= width) continue;
+            if (gears[`${x}x${y}`]) {
+                gears[`${x}x${y}`].push(number.n);
+            }
+        }
+    }
+    y++;
+    if (number.s > 0 && gears[`${number.s - 1}x${y}`]) {
+        gears[`${number.s - 1}x${y}`].push(number.n);
+    }
+    if (number.e < width - 1 && gears[`${number.e + 1}x${y}`]) {
+        gears[`${number.e + 1}x${y}`].push(number.n);
+    }
+    y++;
+    if (y >= height) continue;
+    for (let x = number.s - 1; x <= number.e + 1; x++) {
+        if (x < 0 || x >= width) continue;
+        if (gears[`${x}x${y}`]) {
+            gears[`${x}x${y}`].push(number.n);
+        }
+    }
+}
+
+console.log(Object.values(gears).filter(g => g.length === 2).reduce((s, g) => s + g[0]*g[1], 0))
